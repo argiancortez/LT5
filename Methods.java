239cad92256dd4be6aa0c4fb6fb547e934863a1d
argiancortez <99863214+argiancortez@users.noreply.github.com>
2022-12-03 22:19:55 +0800

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
 
 class Compute extends Methods {
-     public static int computeAge(String value1, String value2) {
+     public static int computeDamage(String value1, String value2) {
          int val1 = Integer.parseInt(value1);
          int val2 = Integer.parseInt(value2);
          int finalage = val1 + val2;
-         return finalage;
+         return finaldamage;
 
      }
 
-     public static void displayMessage(String finalage){
-         JOptionPane.showMessageDialog( null, "Your full damage is: " + finalage, "Result" , JOptionPane.INFORMATION_MESSAGE );
+     public static void displayMessage(String finaldamage){
+         JOptionPane.showMessageDialog( null, "Your full damage is: " + finaldamage, "Result" , JOptionPane.INFORMATION_MESSAGE );
      }
  }
